241b4a2cf07fbc93c6a4c2897ec0ee2c8efec5f1
GomSon-E <gomsonixx@gmail.com>
2025-05-21 13:30:10 +0900

    UPDATE : 얼굴 인식 모델 변경 및 기타 설정 제거

--- config.py
+++ config.py
@@ -6,19 +6,12 @@ CSV_PATH = "face_encodings.csv"
 ATTENDANCE_CSV_PATH = "attendance_records.csv"
 
 # 얼굴 인식 설정
-FACE_MODEL = "Facenet"  # 얼굴 인식 모델
+FACE_MODEL = "ArcFace"  # 얼굴 인식 모델
 DETECTOR_BACKEND = "mtcnn"  # 얼굴 감지 백엔드
 
 # 경로 생성 시 슬래시 사용하는 함수
 def create_path(*args):
     return '/'.join(args)
-
-# 기타 설정
-MIN_FACE_RATIO = 0.20  # 최소 얼굴 크기 비율
-IMAGE_ENHANCEMENT = {
-    "alpha": 1.5,  # 대비 증가 (1.0 = 원본)
-    "beta": 30     # 밝기 증가 (-255 ~ 255)
-}
 
 # 출퇴근 태그 시간 설정
 ATTENDANCE_TIMES = {
